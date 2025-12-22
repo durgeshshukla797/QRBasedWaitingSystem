3653a3cb36f5ac99b30bd73f98d614e39e6a12e5
Durgesh <ds6636297@gmail.com>
2025-12-22 22:17:33 +0530

    intiated the project

---
+++ Backend/index.js
@@ -0,0 +1,28 @@
+import dotenv from 'dotenv'
+dotenv.config({quiet:true});
+
+import express from 'express';
+import cors from 'cors'
+import mongoose from 'mongoose';
+import cookieParser from 'cookie-parser';
+
+
+const app=express();
+
+app.use(cors());
+app.use(express.json())
+app.use(cookieParser());
+
+app.listen(process.env.PORT || 3000,()=>{
+  console.log(`Server is running at port ${process.env.PORT}`);
+})
+
+mongoose.connect(process.env.MONGODB_URL)
+        .then(()=>{
+          console.log("Mongodb Connected");
+        })
+        .catch((err)=>{
+             console.log("Error In Mongodb Connection", err);
+            process.exit(1);
+        })
+
